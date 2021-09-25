669d61dd663c7b691095ed9e8f1cce06744d5d82
sharathkuchal <90147425+sharathkuchal@users.noreply.github.com>
2021-09-25 11:50:25 +0530

    Add files via upload
    procedure script to drop multiple tables with pattern in names

---
+++ drop_table_pattern_procedure.sql
@@ -0,0 +1,18 @@
+-- set table schema and pattern matching for tables
+SET @schema = 'schema_name';
+SET @pattern = 'pattern%';
+
+-- build dynamic sql (DROP TABLE tbl1, tbl2...;)
+SELECT CONCAT('DROP TABLE ',GROUP_CONCAT(CONCAT(@schema,'.',table_name)),';')
+INTO @droplike
+FROM information_schema.tables
+WHERE @schema = database()
+AND table_name LIKE @pattern;
+
+-- display the dynamic sql statement
+SELECT @droplike;
+
+-- execute dynamic sql
+PREPARE stmt FROM @droplike;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
